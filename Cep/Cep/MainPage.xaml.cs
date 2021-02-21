023953517d0d8d99606f5adf4dbdfc34d48db6a4
Fernando de Faria <nandodefaria@yahoo.com>
2021-02-20 23:20:16 -0300

    Add project files.

---
+++ Cep/Cep/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Cep.Service.Model;
+using Cep.Service;
+
+namespace Cep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCEP;
+        }
+        
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim();
+            if (isValid(cep))
+            {
+                try {
+                    Endereco end = ViaCepService.BuscarEnderecoViaCep(cep);
+
+                    if (end != null)
+                    {
+                        RESULTADO.Text = "Endereço para o CEP " + cep + ": "
+                            + end.logradouro + ", "
+                            + end.complemento + ", "
+                            + end.bairro + ", "
+                            + end.localidade + ", "
+                            + end.uf + ".";
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "O endereço não foi encontrado, para o CEP informado: " + cep, "OK");
+                    }
+                }catch(Exception e)
+                {
+                    DisplayAlert("Erro", e.Message, "OK");
+                }
+            }
+
+           
+        }
+        private bool isValid(string cep)
+        {
+            int CepValido = 0;
+            if ((cep.Length != 8) || !int.TryParse(cep, out CepValido))
+            {
+                DisplayAlert("Erro", "O cep deve conter somente os 8 números!", "OK");
+                return false;
+            }
+            
+           return true;
+        }
+    }
+}
---
+++ Cep/Cep/Service/ViaCepService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Cep.Service.Model;
+using Newtonsoft.Json;
+
+namespace Cep.Service
+{
+    public class ViaCepService
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if (end.cep == null) return null;
+            return end;
+        }
+    }
+}
